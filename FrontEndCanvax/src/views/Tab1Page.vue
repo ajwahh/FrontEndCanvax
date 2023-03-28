e6499ba31513c1cdd2389319f323b0272d2f3a38
TabascoTwan <antonlokesandell@gmail.com>
2023-03-28 12:24:11 +0200

    Nytt test Ionic

---
+++ FrontEndCanvax/src/views/Tab1Page.vue
@@ -0,0 +1,24 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+      </ion-header>
+       <ion-grid>
+         <ion-row>
+           <ion-col>Mon</ion-col>
+           <ion-col>Tue</ion-col>
+           <ion-col>Wed</ion-col>
+           <ion-col>Thur</ion-col>
+           <ion-col>Fri</ion-col>
+           <ion-col>Sun</ion-col>
+           <ion-col>Sat</ion-col>
+         </ion-row>
+       </ion-grid>
+     <!-- <ion-datetime first-day-of-week="1"></ion-datetime> -->
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+</script>
---
+++ FrontEndCanvax/src/views/Tab3Page.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+      </ion-header>
+        <ion-list>
+          <ion-item>
+            <ion-button>Kontoinformation</ion-button>
+          </ion-item>
+          <ion-item>
+            <ion-button>Språk och tidsformat</ion-button>
+          </ion-item>
+          <ion-item>
+            <ion-button>Byt schema</ion-button>
+          </ion-item>
+        </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+</script>
